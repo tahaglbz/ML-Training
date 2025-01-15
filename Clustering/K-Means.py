1f8cf5945084f7bed16a37149b0d6171b6ff3e40
tahaglbz <tahaglbz1@gmail.com>
2025-01-16 00:17:52 +0300

    K-means

--- Clustering/K-Means.py
+++ Clustering/K-Means.py
@@ -5,3 +5,25 @@ Created on Wed Jan 15 23:44:25 2025
 @author: tahag
 """
 
+import numpy as np
+import pandas as pd 
+import matplotlib.pyplot as plt
+
+datas = pd.read_csv('musteriler.csv')
+
+X = datas.iloc[:,3:].values
+
+from sklearn.cluster import KMeans
+
+km = KMeans(n_clusters=3,init='k-means++')
+km.fit(X)
+
+print(km.cluster_centers_)
+result=[]
+for i in range(1,11):
+    kmean = KMeans(n_clusters=i,init='k-means++',random_state=123)
+    kmean.fit(X)
+    result.append(kmean.inertia_)
+    
+    
+plt.plot(range(1,11), result)
